f211dbe3be00b602e5a2bfc1c9e3e4bb6e1bbdb1
mohitudupa <33372461+mohitudupa@users.noreply.github.com>
2018-07-09 16:35:12 +0530

    Bug fixes

--- model_testing.py
+++ model_testing.py
@@ -3,42 +3,190 @@ import json
 import os
 
 
-host = "127.0.0.1:80"
+host = "127.0.0.1:8000"
 
+
+instance_id = 0
+
+tada = {
+	"get token": {
+		"url": "/model/api/gettoken/",
+		"data": {
+					"username": str,
+					"password": str,
+		}
+	},
+	"change password": {
+		"url": "/model/api/changepassword/",
+		"data": {
+					"old_password": str,
+					"new_password": str,
+		}
+	},
+	"change token": {
+		"url": "/model/api/changetoken/",
+		"data": {
+					"username": str,
+					"password": str,
+		}
+	},
+	"get details": {
+		"url": "/model/api/getdetails/",
+		"data": {
+					"trash": bool,
+		}
+	},
+	"get names": {
+		"url": "/model/api/getnames/",
+		"data": {
+					"trash": bool,
+		}
+	},
+	"get versions": {
+		"url": "/model/api/getversions/",
+		"data": {
+					"name": str,
+					"trash": bool
+		}
+	},
+	"get instances": {
+		"url": "/model/api/getinstances/",
+		"data": {
+					"name": str,
+					"version": str,
+					"trash": bool
+		}
+	},
+	"get model": {
+		"url": "/model/api/getmodel/",
+		"data": {
+					"id": str
+		}
+	},
+	"get log": {
+		"url": "/model/api/getlog/",
+		"data": {
+					"id": str
+		}
+	},
+	"get user log": {
+		"url": "/model/api/getuserlog/",
+		"data": {}
+	},
+	"get date log": {
+		"url": "/model/api/getdatelog/",
+		"data": {
+					"date": str
+		}
+	},
+	"delete": {
+		"url": "/model/api/delete/",
+		"data": {
+					"id": str
+		}
+	},
+	"restore": {
+		"url": "/model/api/restore/",
+		"data": {
+					"id": str
+		}
+	},
+	"clone": {
+		"url": "/model/api/clone/",
+		"data": {
+					"id": str
+		}
+	},
+	"upload": {
+		"url": "/model/api/upload/",
+		"data": {
+					"name": str,
+					"version": str,
+					"pickle" : str,
+					"private": bool,
+					"docs": str
+		}
+	},
+	"update": {
+		"url": "/model/api/update/",
+		"data": {
+					"id": str,
+					"new_name": str,
+					"new_version": str,
+					"description": str,
+					"new_private": bool,
+					"new_docs": str
+		}
+	},
+	"commit": {
+		"url": "/model/api/commit/",
+		"data": {
+					"id": str,
+					"description": str,
+		}
+	},
+	"discard": {
+		"url": "/model/api/discard/",
+		"data": {
+					"id": str
+		}
+	},
+	"get traceback": {
+		"url": "/model/api/gettraceback/",
+		"data": {
+					"id": str
+		}
+	},
+	"rollback": {
+		"url": "/model/api/rollback/",
+		"data": {
+					"id": str,
+					"index": str,
+		}
+	},
+	"get status": {
+		"url": "/model/api/getstatus/",
+		"data": {
+					"id": str,
+		}
+	},
+}
+
+
+ml = {
+	"train": {
+		"url": "/model/api/train/",
+		"data": {
+					"split": str,
+					"x_train": "",
+					"y_train": "",
+		}
+	},
+	"test": {
+		"url": "/model/api/test/",
+		"data": {
+					"x_test": "",
+					"y_test": "",
+		}
+	},
+	"predict": {
+		"url": "/model/api/predict/",
+		"data": {
+					"x_predict": "",
+		}
+	},
+	"cluster": {
+		"url": "/model/api/cluster/",
+		"data": {
+					"x_cluster": "",
+		}
+	},
+}
 
 def pretty(data):
 	print("{")
 	[print("\t", i, ":", data[i]) for i in data]
 	print("}")
-
-
-def make_call(headers, data, url, action):
-	print("Data sent:")
-	pretty(data)
-	if action != "predict" and action != "test" and action != "train":
-		r = requests.post(url = url, data = json.dumps(data), headers = headers)
-
-	elif action == "predict":
-		multipart_form_data = {'x_predict': open('x.csv', 'r')}
-		r = requests.post(url = url, files=multipart_form_data, data=data, headers={"Authorization": headers["Authorization"]})
-
-	elif action == "test":
-		multipart_form_data = {'x_test': open('x.csv', 'r'), 'y_test': open('y.csv', 'r')}
-		r = requests.post(url = url, files=multipart_form_data, data=data, headers={"Authorization": headers["Authorization"]})
-
-	elif action == "train":
-		multipart_form_data = {'x_train': open('x.csv', 'r'), 'y_train': open('y.csv', 'r')}
-		r = requests.post(url = url, files=multipart_form_data, data=data, headers={"Authorization": headers["Authorization"]})
-
-	print("Result:", r)
-	pretty(r.json())
-
-
-def read_test_data():
-	f = open("test_data.md", 'r')
-	tada = eval(f.read())
-	f.close()
-	return(tada)
 
 
 def read_header_data():
@@ -47,7 +195,7 @@ def read_header_data():
 		f = open("header_data.md", "r")
 		headers["Authorization"] = "token " + f.read().strip()
 	except FileNotFoundError:
-		x = input("Enrter Auth token: ").strip()
+		x = input("Enrter Auth token (<class> 'str'): ").strip()
 		f = open("header_data.md", "w")
 		f.write(x)
 		headers["Authorization"] = "token " + x
@@ -55,9 +203,72 @@ def read_header_data():
 	return headers
 
 
+def get_instance_id():
+	global instance_id
+	if instance_id == 0:
+		instance_id = input("instance ID (<class> 'str'): ")
+	return(instance_id)
+
+
+def change_model():
+	global instance_id
+	instance_id = input("instance ID (<class> 'str'): ")
+
+
+def do_json_action(headers, keytype, url):
+	data = {}
+
+	for key in keytype.keys():
+		if key == "id":
+			data["id"] = get_instance_id()
+		elif keytype[key] == bool:
+			data[key] = eval(input(str(key) + " (" + str(keytype[key]) + ")" + ": "))
+		else:
+			data[key] = keytype[key](input(str(key) + " (" + str(keytype[key]) + ")" + ": "))
+
+	print("Data sent:")
+	pretty(data)
+	
+	r = requests.post(url=url, data=json.dumps(data), headers=headers)
+	
+	print("Result:", str(r)[-5: -2])
+	
+	if str(r)[-5: -2] == "200":
+		pretty(r.json())
+	else:
+		print("error")
+
+
+def do_form_action(headers, keytype, url):
+
+	data ={"id": get_instance_id()}
+
+	files = {}
+
+	for key in keytype.keys():
+		if key == "split":
+			data["split"] = int(input("split (<class> 'str'): "))
+		else:
+			file_name = input("Enter file for " + key + ": ")
+			files[key] = open(file_name, "r")
+
+	print("Data sent:")
+	pretty(data)
+	print("Files sent:")
+	pretty(files)	
+	r = requests.post(url=url, files=files, data=data, headers={"Authorization": headers["Authorization"]})
+	
+	print("Result:", str(r)[-5: -2])
+	
+	if str(r)[-5: -2] == "200":
+		pretty(r.json())
+	else:
+		print("error")
+
+
 def main():
 	headers = read_header_data()
-	tada = read_test_data()
+	global tada, ml
 	while 1:
 		print("-" * os.get_terminal_size().columns)
 		x = input("> ")
@@ -65,16 +276,16 @@ def main():
 			print("List of commands")
 			for i in tada:
 				print(i)
-			print("refresh data\nhelp\n^C")
-			continue
-		if x == "refresh data":
-			tada = read_test_data()
-			headers = read_header_data()
-			print("Test cases and header refreshed")
-			continue
-		if x in tada:
+				print("change model")
+		elif x == "change model":
+			change_model()
+		elif x in tada:
 			print("case:", x)
-			make_call(headers, tada[x]["data"], "http://" + host + tada[x]["url"], x)
+			do_json_action(headers, tada[x]["data"], "http://" + host + tada[x]["url"])
+			print()
+		elif x in ml:
+			print("case:", x)
+			do_form_action(headers, ml[x]["data"], "http://" + host + ml[x]["url"])
 			print()
 		else:
 			print("Invalid action:", x)

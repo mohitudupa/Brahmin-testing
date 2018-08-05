93124341a9e31d6c2b94e3aecf21c9b30554839c
mohitudupa <33372461+mohitudupa@users.noreply.github.com>
2018-08-05 17:41:17 +0530

    Version 3.0
    Bug fixes and updates

--- model_testing.py
+++ model_testing.py
@@ -5,7 +5,11 @@ import os
 
 host = "127.0.0.1:8000"
 
-instance_id = 0
+model_id = 0
+
+template_id = 0
+
+array = "array"
 
 tada = {
 	"get token": {
@@ -35,21 +39,9 @@ tada = {
 					"trash": bool,
 		}
 	},
-	"get names": {
-		"url": "/model/api/getnames/",
-		"data": {
-					"trash": bool,
-		}
-	},
-	"get versions": {
-		"url": "/model/api/getversions/",
-		"data": {
-					"name": str,
-					"trash": bool
-		}
-	},
-	"get instances": {
-		"url": "/model/api/getinstances/",
+	
+	"get model id": {
+		"url": "/model/api/getmodelid/",
 		"data": {
 					"name": str,
 					"version": str,
@@ -59,13 +51,13 @@ tada = {
 	"get model": {
 		"url": "/model/api/getmodel/",
 		"data": {
-					"id": str
+					"model_id": str
 		}
 	},
 	"get log": {
 		"url": "/model/api/getlog/",
 		"data": {
-					"id": str
+					"model_id": str
 		}
 	},
 	"get user log": {
@@ -78,109 +70,151 @@ tada = {
 					"date": str
 		}
 	},
-	"delete": {
-		"url": "/model/api/delete/",
+	"get traceback": {
+		"url": "/model/api/gettraceback/",
 		"data": {
-					"id": str
+					"model_id": str
 		}
 	},
-	"restore": {
-		"url": "/model/api/restore/",
+	"get status": {
+		"url": "/model/api/getstatus/",
 		"data": {
-					"id": str
+					"model_id": str,
 		}
 	},
-	"clone": {
-		"url": "/model/api/clone/",
+	"delete model": {
+		"url": "/model/api/deletemodel/",
 		"data": {
-					"id": str
+					"model_id": str
 		}
 	},
-	"upload": {
-		"url": "/model/api/upload/",
+	"restore model": {
+		"url": "/model/api/restoremodel/",
+		"data": {
+					"model_id": str
+		}
+	},
+	"clone model": {
+		"url": "/model/api/clonemodel/",
+		"data": {
+					"model_id": str
+		}
+	},
+	"upload model": {
+		"url": "/model/api/uploadmodel/",
 		"data": {
 					"name": str,
 					"version": str,
 					"pickle" : str,
-					"private": bool,
 					"docs": str
 		}
 	},
-	"update": {
-		"url": "/model/api/update/",
+	"edit model": {
+		"url": "/model/api/editmodel/",
 		"data": {
-					"id": str,
+					"model_id": str,
 					"new_name": str,
 					"new_version": str,
 					"description": str,
-					"new_private": bool,
 					"new_docs": str
 		}
 	},
 	"commit": {
 		"url": "/model/api/commit/",
 		"data": {
-					"id": str,
+					"model_id": str,
 					"description": str,
 		}
 	},
 	"discard": {
 		"url": "/model/api/discard/",
 		"data": {
-					"id": str
-		}
-	},
-	"get traceback": {
-		"url": "/model/api/gettraceback/",
-		"data": {
-					"id": str
+					"model_id": str
 		}
 	},
 	"rollback": {
 		"url": "/model/api/rollback/",
 		"data": {
-					"id": str,
+					"model_id": str,
 					"index": str,
 		}
 	},
-	"get status": {
-		"url": "/model/api/getstatus/",
+	"abort": {
+		"url": "/model/api/abort/",
 		"data": {
-					"id": str,
+					"model_id": str,
+		}
+	},
+	"get result": {
+		"url": "/model/api/getresult/",
+		"data": {
+					"model_id": str,
+		}
+	},
+	"clone template": {
+		"url": "/model/api/clonetemplate/",
+		"data": {
+					"template_id": str,
+		}
+	},
+	"upload template": {
+		"url": "/model/api/uploadtemplate/",
+		"data": {
+					"name": str,
+					"docs": str,
+					"data": dict,
+		}
+	},
+	"edit template": {
+		"url": "/model/api/edittemplate/",
+		"data": {
+					"template_id": str,
+					"new_name": str,
+					"new_docs": str,
+					"new_data": dict,
+		}
+	},
+	"delete template": {
+		"url": "/model/api/deletetemplate/",
+		"data": {
+					"template_id": str,
+		}
+	},
+	"get template details": {
+		"url": "/model/api/gettemplatedetails/",
+		"data": {}
+	},
+	"get template": {
+		"url": "/model/api/gettemplate/",
+		"data": {
+					"template_id": str,
+		}
+	},
+	"get attribute": {
+		"url": "/model/api/getattribute/",
+		"data": {
+					"model_id": str,
+					"attribute": str,
 		}
 	},
 }
 
 
 ml = {
-	"train": {
-		"url": "/model/api/train/",
+	"exec command": {
+		"url": "/model/api/execcommand/",
 		"data": {
-					"split": str,
-					"x_train": "",
-					"y_train": "",
-		}
-	},
-	"test": {
-		"url": "/model/api/test/",
-		"data": {
-					"x_test": "",
-					"y_test": "",
-		}
-	},
-	"predict": {
-		"url": "/model/api/predict/",
-		"data": {
-					"x_predict": "",
-		}
-	},
-	"cluster": {
-		"url": "/model/api/cluster/",
-		"data": {
-					"x_cluster": "",
+					"model_id": str,
+					"template_id": str,
+					"function": str,
+					"data": dict,
 		}
 	},
 }
+
+def pretty(data):
+	map_json = json.dumps(data, indent=4, sort_keys=True)
+	print(map_json)
 
 
 def login():
@@ -217,31 +251,46 @@ def read_header_data():
 	return headers
 
 
-def get_instance_id():
-	global instance_id
-	if instance_id == 0:
-		instance_id = input("instance ID (<class> 'str'): ")
-	return(instance_id)
+def get_model_id():
+	global model_id
+	if model_id == 0:
+		model_id = input("instance ID (<class> 'str'): ")
+	return(model_id)
+
+
+def get_template_id():
+	global template_id
+	if template_id == 0:
+		template_id = input("template ID (<class> 'str'): ")
+	return(template_id)
 
 
 def change_model():
-	global instance_id
-	instance_id = input("instance ID (<class> 'str'): ")
+	global model_id
+	model_id = input("instance ID (<class> 'str'): ")
+
+
+def change_template():
+	global template_id
+	template_id = input("template ID (<class> 'str'): ")
 
 
 def do_json_action(headers, keytype, url):
 	data = {}
 
 	for key in keytype.keys():
-		if key == "id":
-			data["id"] = get_instance_id()
-		elif keytype[key] == bool:
+		if key == "model_id":
+			data["model_id"] = get_model_id()
+		elif key == "template_id":
+			data["template_id"] = get_template_id()
+		elif keytype[key] in [bool, dict]:
 			data[key] = eval(input(str(key) + " (" + str(keytype[key]) + ")" + ": "))
 		else:
 			data[key] = keytype[key](input(str(key) + " (" + str(keytype[key]) + ")" + ": "))
 
 	print("Data sent:")
 	print(json.dumps(data, indent=4, sort_keys=True))
+	# pretty(data)
 	
 	r = requests.post(url=url, data=json.dumps(data), headers=headers)
 	
@@ -249,35 +298,42 @@ def do_json_action(headers, keytype, url):
 	
 	if str(r)[-5: -2] == "200":
 		print(json.dumps(r.json(), indent=4, sort_keys=True))
+		# pretty(r.json())
 	else:
 		print("error")
 
 
 def do_form_action(headers, keytype, url):
 
-	data ={"id": get_instance_id()}
-
 	files = {}
-	file_data = {}
+	data = {}
 
-	for key in keytype.keys():
-		if key == "split":
-			data["split"] = int(input("split (<class> 'str'): "))
-		else:
-			file_name = input("Enter file for " + key + ": ")
-			files[key] = open(file_name, "r")
-			file_data[key] = str(files[key])
+	while 1:
+		key_name = input("Enter the key name: ")
+		if key_name == "done":
+			break
+		key_type = eval(input("Enter the key type: "))
+		if key_type in [str, int, float]:
+			data[key_name] = key_type(input("Enter the value: "))
+		elif key_type == bool:
+			data[key_name] = eval(input("Enter the value: "))
+		elif key_type == "array":
+			file_name = input("Enter file name: ")
+			files[key_name] = open(file_name, "r")
+
+	data["model_id"] = get_model_id()
+	data["template_id"] = get_template_id()
+	data["function"] = input("Enter the function name: ")
 
 	print("Data sent:")
 	print(json.dumps(data, indent=4, sort_keys=True))
-	print("Files sent:")
-	print(json.dumps(file_data, indent=4, sort_keys=True))
 	r = requests.post(url=url, files=files, data=data, headers={"Authorization": headers["Authorization"]})
 	
 	print("Result:", str(r)[-5: -2])
 	
 	if str(r)[-5: -2] == "200":
 		print(json.dumps(r.json(), indent=4, sort_keys=True))
+		# pretty(r.json())
 	else:
 		print("error")
 
@@ -288,24 +344,29 @@ def main():
 	while 1:
 		print("-" * os.get_terminal_size().columns)
 		x = input("> ")
-		if x == "help":
-			print("List of commands")
-			print("change model")
-			for i in tada:
-				print(i)
-			print("^C")
-		elif x == "change model":
-			change_model()
-		elif x in tada:
-			print("case:", x)
-			do_json_action(headers, tada[x]["data"], "http://" + host + tada[x]["url"])
-			print()
-		elif x in ml:
-			print("case:", x)
-			do_form_action(headers, ml[x]["data"], "http://" + host + ml[x]["url"])
-			print()
-		else:
-			print("Invalid action:", x)
+		try:
+			if x == "help":
+				print("List of commands")
+				print("change model")
+				for i in tada:
+					print(i)
+				print("^C")
+			elif x == "change model":
+				change_model()
+			elif x == "change template":
+				change_template()
+			elif x in tada:
+				print("case:", x)
+				do_json_action(headers, tada[x]["data"], "http://" + host + tada[x]["url"])
+				print()
+			elif x in ml:
+				print("case:", x)
+				do_form_action(headers, ml[x]["data"], "http://" + host + ml[x]["url"])
+				print()
+			else:
+				print("Invalid action:", x)
+		except Exception as e:
+			print(e)
 
 try:
 	if __name__ == "__main__":
